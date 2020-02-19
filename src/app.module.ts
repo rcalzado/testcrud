9955124eecdf1bba0fa9f29d090464cc599baf3f
Rafael Calzado <rcalzado.ti@gmail.com>
2020-02-18 23:40:42 -0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HeroesModule } from './heroes/heroes.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'dba',
+      password: 'mysqladmin',
+      database: 'testcrud',
+      entities: [ __dirname + '/**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
+    HeroesModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/heroes/heroes.controller.ts
@@ -0,0 +1,14 @@
+import { Controller } from '@nestjs/common';
+import { Crud } from '@nestjsx/crud';
+import { Hero } from './hero.entity';
+import { HeroesService } from './heroes.service';
+
+@Crud({
+  model: {
+    type: Hero,
+  },
+})
+@Controller('heroes')
+export class HeroesController {
+  constructor(public service: HeroesService) {}
+}
